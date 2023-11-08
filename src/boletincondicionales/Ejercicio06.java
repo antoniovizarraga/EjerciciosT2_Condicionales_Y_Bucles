fbffa340979e3f676c9e6b548fe21486c6283178
antoniovizarraga <145675344+antoniovizarraga@users.noreply.github.com>
2023-11-08 10:16:25 +0100

    Ejercicio 6 terminado

---
+++ src/boletincondicionales/Ejercicio06.java
@@ -0,0 +1,62 @@
+package boletincondicionales;
+import java.util.Scanner;
+import java.util.Random;
+
+public class Ejercicio06 {
+
+	
+	/* ENTRADA:
+	 * 
+	 * (Caso 1)
+	 * 86
+	 * 
+	 * (Caso 2)
+	 * 50
+	 * 
+	 * SALIDA ESPERADA:
+	 * 
+	 * (Caso 1)
+	 * 
+	 * 
+	 * ¡Acertaste!
+	 * 
+	 * (Caso 2)
+	 * 
+	 * Fallaste... El resultado de la suma era: 117
+	 * 
+	 * SALIDA OBTENIDA:
+	 * 
+	 * (Caso 1)
+	 * 
+	 * 
+	 * ¡Acertaste!
+	 * 
+	 * (Caso 2)
+	 * 
+	 * Fallaste... El resultado de la suma era: 117
+	 * 
+	 *  */
+	public static void main(String[] args) {
+		int userNum = 0;
+		final int RANDOM_NUM_1;
+		final int RANDOM_NUM_2;
+		
+		Scanner sc = new Scanner(System.in);
+		Random rnd = new Random();
+		
+		RANDOM_NUM_1 = rnd.nextInt(1, 99 + 1);
+		RANDOM_NUM_2 = rnd.nextInt(1, 99 + 1);
+
+		System.out.println("He generado dos números. Escribe el resultado de " +
+		"la suma de estos dos números.");
+		System.out.print("\n" + RANDOM_NUM_1 + " + " + RANDOM_NUM_2 + " = ");
+		userNum = sc.nextInt();
+		
+		if(userNum == RANDOM_NUM_1 + RANDOM_NUM_2)
+			System.out.println("¡Acertaste!");
+		else
+			System.out.println("Fallaste... El resultado de la suma era: " +
+			(RANDOM_NUM_1 + RANDOM_NUM_2));
+	}
+
+}
